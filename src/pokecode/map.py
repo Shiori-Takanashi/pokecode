56963f54242a72fedbb0a7dadc24137aec1fcb23
Takanashi <nao69alter@gmail.com>
2026-02-12 14:34:25 +0900

    CORE: version06ブランチにてkml処理を追加

---
+++ src/pokecode/map.py
@@ -0,0 +1,43 @@
+# map.py
+
+import logging
+
+from pokecode.map_process import (
+    parse_kml,
+    find_placemarks,
+    get_names_of_place,
+)
+from pokecode.map_format import format_kml
+from pokecode.io_writing import save_json
+from pokecode.config import ConfigGetter
+from pokecode.logconfig import setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+def format() -> None:
+    setup_logging(logger=logger)
+    cget = ConfigGetter()
+    src = cget.get_map_file()
+    dst = cget.get_output_file("map_format", ext="kml")
+    format_kml(src, dst)
+
+
+def process() -> None:
+    setup_logging(logger=logger)
+    cget = ConfigGetter()
+    filepath = cget.get_map_file()
+    tree = parse_kml(filepath)
+    placemarks = find_placemarks(tree)
+    cities = get_names_of_place(placemarks)
+    save_json(cities, cget.get_output_file("city_from_map", ext="json"))
+    return
+
+
+def main() -> None:
+    format()
+    process()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pokecode/map_process.py
@@ -0,0 +1,64 @@
+# src/pokecode/map_parser.py
+
+import logging
+from pathlib import Path
+
+from lxml import etree
+from lxml.etree import _ElementTree, _Element
+
+from pokecode.logconfig import setup_logging
+
+KML_NS = {"kml": "http://www.opengis.net/kml/2.2"}
+
+logger = logging.getLogger(__name__)
+setup_logging(logger=logger, level="DEBUG")
+
+
+def parse_kml(filepath: Path) -> _ElementTree:
+    return etree.parse(filepath)
+
+
+def find_placemarks(tree: _ElementTree) -> list[_Element]:
+    placemarks = tree.xpath(
+        "//kml:Placemark[starts-with(kml:styleUrl, '#icon')]",
+        namespaces=KML_NS,
+    )
+
+    if not isinstance(placemarks, list):
+        raise TypeError("placemarksがlistでない。")
+
+    return [p for p in placemarks if isinstance(p, _Element)]
+
+
+def get_names_of_place(placemarks: list[_Element]) -> list[str]:
+    names: set[str] = set()
+    for pm in placemarks:
+        text = pm.findtext(
+            "kml:name", namespaces=KML_NS
+        )  # ここはElementPathでOK
+        if text is None:
+            continue
+        name = text.strip()
+        if name:
+            names.add(name)
+    names_sorted = list(sorted(names))
+    return names_sorted
+
+
+def main():
+    from pokecode.config import ConfigGetter
+
+    cget = ConfigGetter()
+    fp = cget.get_map_file()
+    tree = parse_kml(fp)
+    placemarks = find_placemarks(tree)
+    names_of_place = get_names_of_place(placemarks)
+    logger.debug(f"place: {len(names_of_place)}")
+    for index, np in enumerate(names_of_place):
+        if index % 500 != 0:
+            continue
+        logger.debug(f"{index} is {np}")
+
+
+if __name__ == "__main__":
+    main()
